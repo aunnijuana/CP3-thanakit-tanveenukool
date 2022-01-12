a5d65c024bfb1c15d10208143e1a802923e841a2
aunnijuana <thanakit.aun@hotmail.com>
2022-01-12 15:50:47 +0700

    ส่งการบ้าน

---
+++ Exercise5_thanakit_t.py
@@ -0,0 +1,6 @@
+data_one = int(input("Data1 : "))
+data_two = int(input("Data2 : "))
+print(data_one,"+",data_two,"=",data_one + data_two)
+print(data_one,"-",data_two,"=",data_one - data_two)
+print(data_one,"*",data_two,"=",data_one * data_two)
+print(data_one,"/",data_two,"=",data_one / data_two)
