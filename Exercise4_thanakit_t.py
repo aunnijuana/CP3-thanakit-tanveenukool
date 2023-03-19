a6ffc58dfa5de8c9e6421d7dce1fc8afebef98b1
aunnijuana <thanakit.aun@hotmail.com>
2023-03-19 14:38:39 +0700

    Create Exercise4_thanakit_t.py
    ส่งการบ้าน 4

---
+++ Exercise4_thanakit_t.py
@@ -0,0 +1,11 @@
+FECLASS = 3.12
+GBCLASS = 2.89
+ICSCLASS = 3.41
+CP = 3.06
+
+print("---Your Score---")
+print("Foundation English :",FECLASS)
+print("General Business",GBCLASS)
+print("Introduction to Computer System :",ICSCLASS)
+print("Computer Programming",CP)
+
